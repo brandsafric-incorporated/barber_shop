ade05d7fe142f6118d9abb8b2787d06fc1eaa153
Navjot Singh <navisinghnz9@gmail.com>
2019-04-27 02:50:55 +1200

    show services in cards

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,22 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4">
-              <h1>Services</h1>
+        <div class="col-md-10">
+              <h1>Our Services</h1>
 
                   @if( count($services) > 0 )
 
+                    <div class="row">
                     @foreach($services as $service)
 
-                      <div class="card" style="margin-top: 20px;">
-                        <div class="card-header bg-info text-white">{{ $service->title }}</div>
-                        <div class="card-body">
-                          <center><img src="{{ asset('images/barber-tools.png') }}" width="100" height="100"></img></center>
-                          <a href=""><h4>{{ $service->title }} for ${{ $service->price }}</h2></a>
+                      <div class="col-md-4">
+                        <div class="card" style="margin-top: 20px;">
+                          <div class="card-header bg-info text-white">{{ $service->title }}</div>
+                          <div class="card-body">
+                            <center>
+                              <img src="{{ asset('images/barber-tools.png') }}" width="80" height="80"></img>
+                              <a href=""><h4>{{ $service->title }}</h4></a>
+                              Cost ${{ $service->price }}
+                            </center>
+                          </div>
                         </div>
                       </div>
 
+                    @if( ($loop->index+1) %3 == 0 )
+                      </div>
+                      <div class="row">
+                    @endif
+
                     @endforeach
+                    </div>
 
                   @endif
         </div>
