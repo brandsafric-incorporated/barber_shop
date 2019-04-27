d7ff6fc76551b21340249799d8d058823fade343
Navjot Singh <navisinghnz9@gmail.com>
2019-04-27 20:06:20 +1200

    updated landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,9 @@
         <title>Barber Shop</title>
 
         <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -61,15 +63,52 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .btn-my{
+              width: 100%;
+              border-radius: 3px;
+              border-color: #fff;
+              background: #AC0B0B;
+              color: #fff;
+              padding: 5px;
+              border: 2px solid #fff;
+
+            }
+            .btn-my:hover{
+              background: #220303;
+              border-color: #fff;
+            }
+            .h_style{
+              font-weight: 900;
+              color: #fff;
+              text-align: center;
+              font-size: 60px;
+              font-style: normal;
+
+            }
+            .h_style2{
+              font-weight: 700;
+              color: #fff;
+              text-align: center;
+              font-style: italic;
+            }
         </style>
     </head>
     <body background="{{ asset('images/bg.jpg') }}">
+
+
         <div class="flex-center position-ref full-height">
+
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ route('home') }}" style="color: white;">{{ __('Home') }}</a>
+                        <a href="{{ route('services') }}" style="color: white;">{{ __('Our Services') }}</a>
+                        <a href="{{ route('gallery') }}" style="color: white;">{{ __('Gallery') }}</a>
+                        <a href="{{ route('contact') }}" style="color: white;">{{ __('Contact Us') }}</a>
+                        <a href="{{ route('about') }}" style="color: white;">{{ __('About Us') }}</a>
+
                         <a href="{{ route('login') }}"  style="color: white;">Login</a>
 
                         @if (Route::has('register'))
@@ -79,11 +118,26 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md" style="color: white;">
-                    <b>BARBER SHOP</b>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-1"></div>
+              <div class="col-sm-10" style="margin-top:10%;">
+
+                <h1 class="h_style">BARBER SHOP</h1>
+                <h1 class="h_style2">When You Look Good You Feel Good.</h1>
+                <h1 class="h_style">022-123-4567</h1>
+
+                <div class="row">
+                  <div class="col-sm-6">
+                    <a href="{{ route('booking') }}" type="button" class="btn btn-outline-primary btn-lg btn-block btn-my">Get an Appointment</a>
+                  </div>
+                  <div class="col-sm-6">
+                    <a href="{{ route('offers') }}" type="button" class="btn btn-outline-primary btn-lg btn-block btn-my">Check Hot Offers</a>
+                  </div>
                 </div>
-            </div>
+              </div>
+                <div class="col-sm-1"></div>
+          </div>
         </div>
     </body>
 </html>
